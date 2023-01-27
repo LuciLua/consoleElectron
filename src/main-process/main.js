423866dc79f0ca5600c1d9be8dd9144b9bba3dee
LuciLua <luci.lua81@gmail.com>
2023-01-27 20:04:55 -0300

    some commands add

--- src/main-process/main.js
+++ src/main-process/main.js
@@ -8,7 +8,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 500,
     autoHideMenuBar: true, // remove menuBar from top
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
